5c0a50d2e50351e059952396fdbf8a7a409922d9
goodee <goodee@DESKTOP-BKMGT3I>
2018-11-01 16:05:19 +0900

    구구단을 외자

--- Sample/src/Sample.java
+++ Sample/src/Sample.java
@@ -5,6 +5,13 @@ public class Sample {
 		System.out.println("앙기모띠");
 		
 		System.out.println("띠모기앙");
+		
+		for(int i = 0 ; i < 10 ; i++) {
+			for(int j = 0 ; j< 10 ; j++ ) {
+				System.out.println(i + "*" + j + "=" + "i*j");				
+			}
+		}
 	}
 
 }
+
