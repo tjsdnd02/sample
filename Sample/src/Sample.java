7e7ee51ce6370c08d97af7eb7bcab1885ced7ee2
goodee <goodee@DESKTOP-BKMGT3I>
2018-11-01 16:08:04 +0900

    구구단을 외자ㅎ...

--- Sample/src/Sample.java
+++ Sample/src/Sample.java
@@ -7,8 +7,8 @@ public class Sample {
 		System.out.println("띠모기앙");
 		
 		for(int i = 2 ; i < 10 ; i++) {
-			for(int j = 2 ; j< 10 ; j++ ) {
-				System.out.println(i + "*" + j + "=" + i*j);			
+			for(int j = 1 ; j< 10 ; j++ ) {
+				System.out.println(i + "*" + j + "=" + i*j);
 			}
 		}
 	}
